0e2402333ebacdcdb58be3dcbdc2408115a9cf37
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-03-27 10:42:14 +0900

    auto-openを実装。非同期が多くなってきたのでasyncで整理。

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var fs        = require('fs'),
-    http      = require('http'),
-    express   = require('express'),
-    emptyPort = require('empty-port'),
-    colors    = require('colors');
+var fs            = require('fs'),
+    async         = require('async'),
+    http          = require('http'),
+    express       = require('express'),
+    emptyPort     = require('empty-port'),
+    colors        = require('colors'),
+    child_process = require('child_process');
 
-var Koko = function (root) {
+var Koko = function (root, opt) {
+    var app = express();
+    var port, url;
+
     colors.setTheme({
-        info: 'green',
-        warn: 'yellow',
-        error: 'red'
+        info  : 'green',
+        warn  : 'yellow',
+        error : 'red'
     });
 
     if (!fs.existsSync(root)) {
@@ -18,21 +23,32 @@ var Koko = function (root) {
 
     console.log('document root\t: %s'.info, root);
 
-    var app = express();
     app.configure(function(){
         app.use(express.static(root));
     });
 
-    emptyPort({}, function (err, port) {
-        if (err) {
-            console.error('Error on picking port. Retry.'.error);
-            process.exit();
+    async.waterfall([function (next) {
+        emptyPort({}, next);
+    }, function (p, next) {
+        port = p;
+        url = 'http://localhost:' + port;
+
+        http.createServer(app).listen(port, next);
+    }, function (next) {
+        console.log('[listen %d]'.info, port);
+
+        if (!opt.autoOpen) {
+            return next();
         }
 
-        http.createServer(app).listen(port, function(){
-            console.log('listen port\t: %d'.info, port);
-        });
-    } );
+        console.log('[open %s]'.info, url);
+        child_process.exec('open ' + url, next);
+    }], function (err) {
+            if (err) {
+                console.error((err + '').error);
+                process.exit();
+            }
+    });
 };
 
 module.exports = Koko;

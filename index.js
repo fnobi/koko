c9f5a7c1c00dd99f2e0b920e47676b6b8f5cd6c1
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-04-06 21:19:04 +0900

    proxyを実装。すこしリファクタリング。

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var fs            = require('fs'),
     async         = require('async'),
+    url           = require('url'),
     http          = require('http'),
     express       = require('express'),
     emptyPort     = require('empty-port'),
+    httpProxy     = require('http-proxy'),
     colors        = require('colors'),
     child_process = require('child_process');
 
 var Koko = function (root, opt) {
-    var app = express();
-    var port, url;
+    var self = this;
+    var app  = express();
+
+    var proxyURL = opt.proxyURL;
+    var autoOpen = opt.autoOpen;
+
+    var port, proxy;
 
     colors.setTheme({
         info  : 'green',
@@ -23,39 +30,82 @@ var Koko = function (root, opt) {
 
     console.log('document root\t: %s'.info, root);
 
+    if (proxyURL) {
+        proxy = new Koko.Proxy(proxyURL);
+        console.log('proxy\t: %s:%d'.info, proxy.host, proxy.port);
+    }
+
     app.configure(function(){
         app.use(express.static(root));
+        app.use(function (req, res, next) {
+            if (!proxy) {
+                return next();
+            }
+            proxy.proxyRequest(req, res);
+        });
     });
 
     async.waterfall([function (next) {
         emptyPort({}, next);
     }, function (p, next) {
         port = p;
-        url = 'http://localhost:' + port;
+        self.port = port;
 
         http.createServer(app).listen(port, next);
+
     }, function (next) {
         console.log('[listen %d]'.info, port);
-
-        var autoOpen = opt.autoOpen;
 
         if (!autoOpen) {
             return next();
         }
 
-        var openURL = [
-            url,
-            autoOpen.replace ? autoOpen.replace(/^\//, '') : ''
-        ].join('/');
+        self.open(autoOpen, next);
 
-        console.log('[open %s]'.info, openURL);
-        child_process.exec('open ' + openURL, next);
     }], function (err) {
             if (err) {
                 console.error((err + '').error);
                 process.exit();
             }
     });
+};
+
+Koko.prototype.open = function (openPath, callback) {
+    var port = this.port;
+
+    var openURL = [
+        'http://localhost:' + port,
+        openPath.replace ? openPath.replace(/^\//, '') : ''
+    ].join('/');
+
+    console.log('[open %s]'.info, openURL);
+    child_process.exec('open ' + openURL, callback);
+};
+
+Koko.Proxy = function (proxyURL) {
+    var host = url.parse(proxyURL).hostname || 'localhost';
+    var port = url.parse(proxyURL).port     || 80;
+
+    var proxy = new httpProxy.HttpProxy({
+        target: {
+            host: host,
+            port: port
+        }
+    });
+
+    this.url = proxyURL;
+
+    this.host = host;
+    this.port = port;
+    this.proxy = proxy;
+};
+
+Koko.Proxy.prototype.proxyRequest = function (req, res) {
+    var host  = this.host;
+    var proxy = this.proxy;
+
+    req.headers.host = host;
+    proxy.proxyRequest(req, res);
 };
 
 module.exports = Koko;
